bc10799f8ec62b6bb3620bfa1a41163b6c80380c
leand <leand@Leandrus>
2024-11-17 19:00:49 -0300

    criaçao do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+  
+	/*main é um método auto executável em java*/
+	public static void main(String[] args) {
+		
+		/*escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java - do Curso Formação java Web");
+		System.out.println("Java é orientado a objetos");
+		}
+}
+
